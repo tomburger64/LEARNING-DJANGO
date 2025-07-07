ad0ef03b942c2a154f30e52ac7926483822e27cf
tomburger64 <tom64bonde@gmail.com>
2025-07-07 20:29:15 +0200

    simple db & model migrated

--- TechWithTimTutorial/demo/myapp/admin.py
+++ TechWithTimTutorial/demo/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TodoItem #import the basic todo db model done in models.py
 
 # Register your models here.
+# models are the DB stuff (like the rows and fields attributes I made in models.py)
+admin.site.register(TodoItem)
+# any change done to the db models requires a "migration"
+# a migration's like updating the db while keeping its existing data
+# to do that, use CLI (https://youtu.be/nGIg40xs9e4?si=j1N4YRzGq5SdUUj8&t=932)
--- TechWithTimTutorial/demo/myapp/models.py
+++ TechWithTimTutorial/demo/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# this is made to create DBs using code (ORM)
+# converts the code into set schemes for supported DBs (SQLite, Potgre, Mongo, etc)
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200) #title row, contains a char field
+    completed = models.BooleanField(default=False) # completed row, contains bool field
+# this needs to be registered in the "admin panel" once done
