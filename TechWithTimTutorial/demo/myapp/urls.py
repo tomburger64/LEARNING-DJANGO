13f0d1192d2e4eed21effc64810bafb9b26e64b6
tomburger64 <tom64bonde@gmail.com>
2025-07-07 21:26:44 +0200

    render of some db entries (no items yet)

--- TechWithTimTutorial/demo/myapp/urls.py
+++ TechWithTimTutorial/demo/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 # when we go to specified path (` "" `) (url)
 # runs the views.home view or function (myapp\views.py) which returns an http response
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("todos/", views.todos, name='Todos')
 ]
--- TechWithTimTutorial/demo/myapp/views.py
+++ TechWithTimTutorial/demo/myapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem # importing todos so it can be passed
 
-# # ↓ can return html template or http
+# # ↓ can return html template or some http
 # def home(request):
 #     return HttpResponse("Hello World!")
 
+# function that takes a "request" parameter (HTTP request)
+# return render function, returns HTTP response
+# the HTTP
 def home(request):
     return render(request, "home.html")
+
+# passing a dictionnary to the render returnto see the vars that hold the db entries
+# instance var items made to get all the model's data (functions from django, not classic dom)
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos.html": items})
