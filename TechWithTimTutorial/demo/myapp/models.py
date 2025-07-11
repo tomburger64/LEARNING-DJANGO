0d9edf0660675f7dadcf69271ef52d1cdc1bd195
tomburger64 <tom64bonde@gmail.com>
2025-07-11 16:01:45 +0200

    some more commenting, and go next project

--- TechWithTimTutorial/demo/myapp/models.py
+++ TechWithTimTutorial/demo/myapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models (databases) here.
 # this is made to create DBs using code (ORM)
 # converts the code into set schemes for supported DBs (SQLite, Potgre, Mongo, etc)
 
+# todoitem db
 class TodoItem(models.Model):
     title = models.CharField(max_length=200) #title row, contains a char field
     completed = models.BooleanField(default=False) # completed row, contains bool field
--- TechWithTimTutorial/demo/myapp/views.py
+++ TechWithTimTutorial/demo/myapp/views.py
@@ -7,11 +7,10 @@ from .models import TodoItem # importing todos so it can be passed
 
 # function that takes a "request" parameter (HTTP request)
 # return render function, returns HTTP response
-# the HTTP
 def home(request):
     return render(request, "home.html")
 
-# passing a dictionnary to the render returnto see the vars that hold the db entries
+# passing a dictionnary as render's param, goes through the db entries
 # instance var items made to get all the model's data (functions from django, not classic dom)
 def todos(request):
     items = TodoItem.objects.all()
