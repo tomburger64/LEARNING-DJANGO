b536ae3a96fc09e4710396ad9d90ca41b487bc85
tomburger64 <tom64bonde@gmail.com>
2025-07-11 15:40:09 +0200

    first time in admin panel ("admin/" url), added 2 todo items in the todos db

--- TechWithTimTutorial/demo/myapp/urls.py
+++ TechWithTimTutorial/demo/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# LOGIN FOR "admin/" IS USER (for every field, mail empty)
+
 # when we go to specified path (` "" `) (url)
 # runs the views.home view or function (myapp\views.py) which returns an http response
 urlpatterns = [
